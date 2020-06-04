96033e2f25b7c2525a6abffce22fd3274bf1ff47
Ezequiel Suchmon <ezequielsuchmon@iMac-de-Ezequiel.local>
2020-06-04 15:11:08 -0300

    reemplazando archivos

---
+++ js/funciones.js
@@ -0,0 +1,9 @@
+console .log (nombre)
+ let culpable
+   prompt culpable = si o no 
+    if (culpable= si)
+    {console.log ('es positivo')
+     }
+    else {
+        console. log ('es negativo')
+    }
